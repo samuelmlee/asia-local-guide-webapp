40226230dd4d2575764f8145a74aea66d044edda
Samuel Lee <sam.leefr@gmail.com>
2024-12-09 22:25:30 +0100

    Draft date picker

--- src/app/core/landing/responsive-search/mobile-search-dialog/mobile-search-dialog.component.ts
+++ src/app/core/landing/responsive-search/mobile-search-dialog/mobile-search-dialog.component.ts
@@ -1,6 +1,14 @@
 import { ChangeDetectionStrategy, Component, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_NATIVE_DATE_FORMATS,
+  MatNativeDateModule,
+  NativeDateAdapter,
+} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +17,8 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-mobile-search-dialog',
   imports: [
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatDialogModule,
     MatButtonModule,
     MatFormFieldModule,
@@ -16,10 +26,18 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule,
     FormsModule,
   ],
+  providers: [
+    { provide: DateAdapter, useClass: NativeDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS },
+  ],
   templateUrl: './mobile-search-dialog.component.html',
   styleUrl: './mobile-search-dialog.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MobileSearchDialogComponent {
-  public searchTerm = model('');
+  public searchQuery = model('');
+
+  public startDate = new Date();
+
+  public submitSearch(): void {}
 }
