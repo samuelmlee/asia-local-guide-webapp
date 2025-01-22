39622951e76385666bc2aa2c052a35cd66bb1581
Samuel Lee <sam.leefr@gmail.com>
2025-01-22 23:56:40 +0100

    Draft language selector

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule],
+  imports: [MatIconModule, LanguageSelectorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/core/components/language-selector/language-selector.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-language-selector',
+  imports: [MatMenuModule, MatIconModule],
+  templateUrl: './language-selector.component.html',
+  styleUrl: './language-selector.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LanguageSelectorComponent {
+  public selectedLanguage = signal('FR');
+
+  public selectLanguage(language: string): void {
+    this.selectedLanguage.set(language);
+    // Add additional logic updating a translation service
+  }
+}
