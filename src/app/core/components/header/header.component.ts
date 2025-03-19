198f1bcca0fc5d435f4d16f7774bc48286b03df8
Samuel Lee <sam.leefr@gmail.com>
2025-03-19 21:18:22 +0100

    sidenav draft

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+import { Subject } from 'rxjs';
+import { SideNavComponent } from '../side-nav/side-nav.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, LanguageSelectorComponent],
+  imports: [MatIconModule, SideNavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly toggleSub = new Subject<void>();
+
+  public readonly toggleObs = this.toggleSub.asObservable();
+
+  public toggleSidenav(): void {
+    this.toggleSub.next();
+  }
+}
