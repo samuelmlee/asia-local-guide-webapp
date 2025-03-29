dfa5a70e5017d5e92b5b05559a4896bfe822cd9d
Samuel Lee <sam.leefr@gmail.com>
2025-03-29 23:08:21 +0100

    add router link to header

--- src/app/core/components/simple-header/simple-header.component.ts
+++ src/app/core/components/simple-header/simple-header.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-simple-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './simple-header.component.html',
   styleUrl: './simple-header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SimpleHeaderComponent {
-
-}
+export class SimpleHeaderComponent {}
