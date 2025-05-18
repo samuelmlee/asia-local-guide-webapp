d97458f79934e9f16ff897e72f630c6cf76d322c
Samuel Lee <sam.leefr@gmail.com>
2025-05-19 00:02:38 +0200

    Draft scroll change

--- src/app/features/planning/components/planning/planning.component.ts
+++ src/app/features/planning/components/planning/planning.component.ts
@@ -2,21 +2,30 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
-  TemplateRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
 } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { debounceTime, Subject } from 'rxjs';
 import { ResponsiveSearchComponent } from '../../../search/components/responsive-search/responsive-search.component';
 import { PlanningService } from '../../services/planning.service';
 import { PlanningDayComponent } from '../planning-day/planning-day.component';
 
 @Component({
   selector: 'app-planning',
-  imports: [ResponsiveSearchComponent, PlanningDayComponent, MatIconModule],
+  imports: [
+    ResponsiveSearchComponent,
+    PlanningDayComponent,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './planning.component.html',
   styleUrl: './planning.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlanningComponent {
+export class PlanningComponent implements OnInit, OnDestroy {
   public readonly destination = computed(() => {
     const planning = this.planningService.planning();
     return planning?.destination ?? 'Unknown Destination';
@@ -38,11 +47,27 @@ export class PlanningComponent {
     return planning?.dayPlans ?? [];
   });
 
-  public resultTemplate!: TemplateRef<unknown>;
+  private scroll = new Subject<number>();
+
+  public ngOnInit(): void {
+    this.scroll
+      .pipe(debounceTime(200))
+      .subscribe((y) => this.dealWithScroll(window.scrollY));
+  }
+
+  public ngOnDestroy(): void {
+    this.scroll.complete();
+  }
+
+  @HostListener('window:scroll') public watchScroll(): void {
+    this.scroll.next(window.scrollY);
+  }
 
   constructor(private readonly planningService: PlanningService) {}
 
   public savePlanning(): void {
     this.planningService.savePlanning();
   }
+
+  private dealWithScroll(y: number): void {}
 }
